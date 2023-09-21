50e2d2a7049c824f24f68ee928ed6ff07d41caac
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-21 17:43:36 +0900

    update: call api for fetching each article and modify image path

--- src/app/components/Organisms/ArticleList.tsx
+++ src/app/components/Organisms/ArticleList.tsx
@@ -12,7 +12,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
         <article className='flex flex-col shadow my-4' key={article.id}>
           <a href={`articles/${article.id}`} className='hover:opacity-75'>
             <Image
-              src='https://source.unsplash.com/collection/1346951/1000x500?sig=1'
+              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
               alt=''
               width={1280}
               height={300}

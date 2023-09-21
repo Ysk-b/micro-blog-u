825b1beae038f9c178aefd92efb6be784e39e5f4
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-21 17:08:44 +0900

    add: add loading ui

--- src/app/api/blogApi.ts
+++ src/app/api/blogApi.ts
@@ -1,4 +1,5 @@
 // 全記事取得のAPI
+import { resolve } from 'path';
 import { ArticleProps } from '../types/types';
 
 // API作成
@@ -13,7 +14,8 @@ export const getAllArticles = async (): Promise<ArticleProps[]> => {
     throw new Error('エラーが発生しました');
   }
 
-  const articles = await res.json();
+  await new Promise((resolve) => setTimeout(resolve, 1200));
 
+  const articles = await res.json();
   return articles;
 };

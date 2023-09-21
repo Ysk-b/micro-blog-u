bd85551529e301b6a5dbf22f42854dbf9086d8ed
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-21 17:33:22 +0900

    add: create api for fetching each article

--- src/app/api/blogApi.ts
+++ src/app/api/blogApi.ts
@@ -1,13 +1,12 @@
-// 全記事取得のAPI
-import { resolve } from 'path';
 import { ArticleProps } from '../types/types';
-
-// API作成
-// 非同期関数は通常Promiseオブジェクトを返す -> 返すオブジェクトの型定義
+import {notFound} from 'next/navigation'
+ 
+// ➀ 全記事取得のAPI
 export const getAllArticles = async (): Promise<ArticleProps[]> => {
   // SSR -> cache: no-store
   // SSG -> cache: force-cache(default setting)
   // ISR -> next: {revalidate: 10} revalidate: duration
+  // 非同期関数は通常Promiseオブジェクトを返す -> 返すオブジェクトの型定義
   const res = await fetch('http://localhost:3001/posts', { cache: 'no-store' });
 
   if (!res.ok) {
@@ -18,4 +17,25 @@ export const getAllArticles = async (): Promise<ArticleProps[]> => {
 
   const articles = await res.json();
   return articles;
+};
+
+// ➁ 詳細記事取得のAPI
+export const getEachArticle = async (id: string): Promise<ArticleProps> => {
+  // zenn.dev/cybozu_frontend/articles/next-caching-revalidate
+  // 更新頻度低 -> ISR
+  const res = await fetch(`http://localhost:3001/posts/${id}`, { next: { revalidate: 60 } });
+
+  // 404ページ遷移
+  if (res.status === 404) {
+    notFound()
+  }
+
+  if (!res.ok) {
+    throw new Error('エラーが発生しました');
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1200));
+
+  const article = res.json();
+  return article;
 };

03a817c649c2e9b74abe9ba7efce9a0bc5f7fb03
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-22 09:12:48 +0900

    update: move api.ts under pages/api directory

--- src/app/pages/api/api.ts
+++ src/app/pages/api/api.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { supabase } from '../utils/supabaseClient';
+import { supabase } from '../../utils/supabaseClient';
 
 // supaBaseに存在する全記事を取得
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
